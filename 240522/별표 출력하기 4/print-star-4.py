33cbba524160a60e99573643c46fcebbcb00283e
BlueSEO <ssh5623@naver.com>
2024-05-22 07:55:42 +0900

    Title: 별표 출력하기 4, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240522/별표 출력하기 4/print-star-4.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+cnt = 0
+for i in range (n * 2 - 1):
+    for j in range(n - cnt):
+        print('*', end=' ')
+    print()
+
+    if i < n-1:
+        cnt += 1
+    else:
+        cnt -= 1
