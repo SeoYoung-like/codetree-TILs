006e58c40c3bb0d6e4f614be7bf4b837625335c8
BlueSEO <ssh5623@naver.com>
2024-05-23 21:19:06 +0900

    Title: 별표 출력하기 12, Time: 109ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240523/별표 출력하기 12/print-star-12.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+if n == 1:
+    print('*')
+elif n % 2 == 0:
+    for i in range(n):
+        for j in range(n):
+            if i == 0 or j % 2 != 0:
+                print('*', end=' ')
+            else:
+                print(' ', end=' ')
+        print()
+else:
+    for i in range(n-1):
+        for j in range(n):
+            if i == 0 or j % 2 != 0:
+                print('*', end=' ')
+            else:
+                print(' ', end=' ')
