72f9f2de97b6a5163cf9e768f35211d7d9f96f6b
BlueSEO <ssh5623@naver.com>
2024-05-27 07:08:12 +0900

    Title: 학점 계산기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240527/학점 계산기/credit-calculator.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(float, input().split()))
+
+avg = int((sum(arr) / n) * 10 + 0.5) / 10
+
+print(avg)
+if avg >= 4.0:
+    print("Perfect")
+elif avg >= 3.0:
+    print("Good")
+else:
+    pritn("Poor")
