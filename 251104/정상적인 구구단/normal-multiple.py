eafca51fc1b670577f775622cde0c9b667146769
BlueSEO <ssh5623@naver.com>
2025-11-04 08:27:34 +0900

    Title: 정상적인 구구단, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251104/정상적인 구구단/normal-multiple.py
+++ 251104/정상적인 구구단/normal-multiple.py
@@ -3,7 +3,7 @@ n = int(input())
 for i in range(1, n+1):
     for j in range(1,n+1):
         print(f"{i} * {j} = {i*j}", end="")
-        if j != 4:
+        if j != n:
             print(", ", end="")
     print()
 
