45479ed74a81fa602395ebc6c032d2323d96c0e2
BlueSEO <ssh5623@naver.com>
2025-11-03 18:38:26 +0900

    Title: 문자열의 특정 위치 찾기 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251103/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.py
@@ -0,0 +1,11 @@
+str_list = {"apple", "banana", "grape", "blueberry", "orange"}
+ch = input()
+
+count = 0
+for elem in str_list:
+    if elem[2] == ch:
+        count += 1
+    if elem[3] == ch:
+        count += 1
+
+print(count)
