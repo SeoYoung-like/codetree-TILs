2995c842922e6a19345c2180145adebd7e3be24e
BlueSEO <ssh5623@naver.com>
2025-11-05 18:24:44 +0900

    Title: 좌우로 반복하며 출력, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251105/좌우로 반복하며 출력/print-in-left-right-reverse.py
+++ 251105/좌우로 반복하며 출력/print-in-left-right-reverse.py
@@ -1,9 +1,8 @@
 n = int(input())
 arr = [i + 1 for i in range(n)]
 
-for _ in range(4):
-    for i in range(4):
+for _ in range(n):
+    for i in range(n):
         print(arr[i], end="")
     print()
     arr.reverse()
-    
