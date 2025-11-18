2d9d9f542b2df73899e1a3079e333cf9c34095a2
BlueSEO <ssh5623@naver.com>
2025-11-18 09:03:27 +0900

    Title: 달에서 무게 구하기, Time: 55ms, Memory: 10MB, Status: Passed - Codetree

--- 251118/달에서 무게 구하기/weight-on-the-moon.js
+++ 251118/달에서 무게 구하기/weight-on-the-moon.js
@@ -1,4 +1,5 @@
 let balance_weight = 13
 let moon_weight = 0.165
+let total = balance_weight * moon_weight
 
-console.log(`${balance_weight} * ${moon_weight} = ${balance_weight * moon_weight}`)
+console.log(`${balance_weight} * ${moon_weight.toFixed(6)} = ${total.toFixed(6)}`)
